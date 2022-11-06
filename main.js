a6a3d76f6b4c719b839706e683c5c86413a9939f
EdDiass <eduardodias2444@gmail.com>
2022-11-06 17:26:01 -0300

    alterações html - adição js

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector("#app").innerHTML = `
+    <header>
+        <img src="./assets/logo.svg" alt="logo nlw" />
+      </header>
+      <main id="cards">
+        <div class="cards">
+          <h2>24/11 <span>quinta</span></h2>
+          <ul>
+            <li>
+              <img src="./assets/icon-brazil.svg" alt="" />
+              <strong>16:00</strong>
+              <img src="./assets/icon-serbia.svg" alt="" />
+            </li>
+
+          
+          </ul>
+        </div>
+      </main>
+`
