befeb0e1b9d2ba486486cd2a6ed66f0ce86dffd3
EdDiass <eduardodias2444@gmail.com>
2022-11-07 00:51:54 -0300

    concatenação

--- main.js
+++ main.js
@@ -19,21 +19,7 @@ function createCard(data, dia, jogos) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="./assets/logo.svg" alt="logo" />
-      </header>
-      <main id="cards">
-       ${createCard("24/11", "quinta", createJogo("brazil", "16:00", "serbia"))}
-       ${createCard(
-         "28/11",
-         "segunda",
-         createJogo("brazil", "13:00", "switzerland")
-       )}
-       ${createCard(
-         "02/11",
-         "sexta",
-         createJogo("brazil", "16:00", "cameroon")
-       )}
-      </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createJogo("brazil", "16:00", "serbia")) +
+  createCard("28/11", "segunda", createJogo("brazil", "13:00", "switzerland")) +
+  createCard("02/11", "sexta", createJogo("brazil", "16:00", "cameroon"))
